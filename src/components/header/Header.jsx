f195aece1af67a0d486b59a4bc5c349f2d3e490d
Ruthger <ruthger.degraaf@print.com>
2024-06-18 17:10:02 +0200

    More changes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { HomeIcon, FavoriteIcon, CollectionIcon, SettingsIcon, SunIcon, MoonIcon } from '../../icons/Icon';
 import './Header.scss';
 
 const Header = ({ darkMode, toggleDarkMode }) => {
@@ -19,6 +20,9 @@ const Header = ({ darkMode, toggleDarkMode }) => {
                 <h1>#WelcomeUser#</h1>
                 <div>UserImage</div>
             </div>
+            <button className="dark-mode-button" onClick={toggleDarkMode}>
+                {darkMode ? <SunIcon /> : <MoonIcon />}
+            </button>
             <nav>
                 <div 
                     className="dropdown-button" 
@@ -28,16 +32,13 @@ const Header = ({ darkMode, toggleDarkMode }) => {
                     Go 2
                     {isOpen && (
                         <ul className="dropdown">
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to="/favorite">Favorite</Link></li>
-                            <li><Link to="/collection">Collection</Link></li>
-                            <li><Link to="/settings">Settings</Link></li>
+                            <li><Link to="/"><HomeIcon />Home</Link></li>
+                            <li><Link to="/favorite"><FavoriteIcon />Favorite</Link></li>
+                            <li><Link to="/collection"><CollectionIcon />Collection</Link></li>
+                            <li><Link to="/settings"><SettingsIcon />Settings</Link></li>
                         </ul>
                     )}
                 </div>
-                <button onClick={toggleDarkMode}>
-                    {darkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode'}
-                </button>
             </nav>
         </header>
     );
