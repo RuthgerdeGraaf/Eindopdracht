705e7852fa2c56b4b2c88831fe1c492bcd1a95f4
Ruthger <ruthger.degraaf@print.com>
2024-06-19 17:04:33 +0200

    Updated the PS page

--- src/pages/playstation/PlaystationPage.jsx
+++ src/pages/playstation/PlaystationPage.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import onePlayerImage from '../../img/OnePlayer.jpeg';
+import twoPlayerImage from '../../img/TwoPlayers.jpeg';
+import playstationImage from '../../img/Playstation.jpeg';
 
-const PlayStation = () => {
-    return (
+const Home = () => {
+  return (
+    <div>
+      <h2>How many players are you joining with ?</h2>
         <div>
-            <h1>How many players are you with:</h1>
-        {/* <Link to="/playstation">
-            <img src={playstationImage} alt="playstation" width="200px" height="200px" />
-        </Link>
-            <Link to="/playstation">
-        <img src={playstationImage} alt="playstation" width="200px" height="200px" />
-      </Link> */}
+          <Link to="/playstationOnePlayer">
+            <img src={onePlayerImage} className='round-image' alt="OnePlayer" />
+            </Link>
+            <Link to="/playstationTwoPlayers">
+                <img src={twoPlayerImage} className='round-image' alt="TwoPlayers"/>
+            </Link>
         </div>
-    );
+        <div>
+          <img src={playstationImage} className='small-round-image' alt="Playstation"/>
+        </div>
+    </div>
+  );
 };
 
-export default PlayStation;
+export default Home;
